add066291fff811e3eef416d4b7f14069ee43c0a
William Wang <65054034+MemeMasterJeff@users.noreply.github.com>
2022-08-23 15:10:24 -0500

    first stuff in class
    first stuff

---
+++ first(bluej)/ProjectClass.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class ProjectClass here.
+ *
+ * @author William Wang
+ * @version 8-23-22
+ * chapter 1 notes
+ * class notes on basic concepts
+ */
+public class ProjectClass
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(String[] args) {
+        // basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char character = 'f';
+        
+        //classes and objects
+        //Class  ID      memory constructor
+        Integer n1 = new Integer(47);
+        String word = new String("will wang");
+        Double num = new Double(6.7);
+        
+        System.out.print(word.length());
+    
+    }
+}
