7188be0c8be7085349560ef147a3736e19a2d6dc
William Wang <wxwang44@gmail.com>
2022-08-26 02:23:10 -0500

    added in my summer programs

---
+++ summer programs/FtoC.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FtoC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double temp;
+
+        System.out.println("enter premature in F: ");
+        temp = input.nextDouble();
+        System.out.println(temp + " in C is: " + ((temp - 32) * (5.0 / 9)));
+    }
+}
---
+++ summer programs/ReadNums.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReadNums {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.println("enter 3 integers: ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+    }
+}
